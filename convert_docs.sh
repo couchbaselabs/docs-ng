a12866acefbb512e181769c259f7b965e2ba7995
Karen Zeller <karenzeller8@gmail.com>
2013-04-20 12:33:34 -0700

    shell script to prepare docbook, convert to markdown and put in correct content folders

---
+++ convert_docs.sh
@@ -0,0 +1,188 @@
+cd /Users/kzeller/Couchbase_docs/docs
+git pull
+
+#cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-manual-1.8
+#make depend
+#make validate
+#make reformat
+#make couchbase-manual.pdf
+#cd ~/Desktop/docs-ng/
+#java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-manual-1.8/couchbase-manual-ready.xml ccouchbase-manual-1.8
+
+#cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-manual-2.0
+#make depend
+#make validate
+#make reformat
+#make couchbase-manual.pdf
+#cd ~/Desktop/docs-ng/
+#java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-manual-2.0/couchbase-manual-ready.xml couchbase-manual-2.0
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-devguide-1.8
+make depend
+make validate
+make reformat
+make couchbase-devguide.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-devguide-1.8/couchbase-devguide-ready.xml couchbase-devguide-1.8
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-devguide-2.0
+make depend
+make validate
+make reformat
+make couchbase-devguide.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-devguide-2.0/couchbase-devguide-ready.xml couchbase-devguide-2.0
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-java-1.0
+make depend
+make validate
+make reformat
+make couchbase-sdk-java.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-java-1.0/couchbase-sdk-java-ready.xml couchbase-sdk-java-1.0
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-java-1.1
+make depend
+make validate
+make reformat
+make couchbase-sdk-java.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-java-1.1/couchbase-sdk-java-ready.xml couchbase-sdk-java-1.1
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-elastic-search
+make depend
+make validate
+make reformat
+make couchbase-elastic-search.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-elastic-search/couchbase-elastic-search-ready.xml couchbase-elastic-search
+
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-c-1.0
+make depend
+make validate
+make reformat
+make couchbase-sdk-c.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-c-1.0/couchbase-sdk-c-ready.xml couchbase-sdk-c-1.0
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-c-2.0
+make depend
+make validate
+make reformat
+make couchbase-sdk-c.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-c-2.0/couchbase-sdk-c-ready.xml couchbase-sdk-c-2.0
+
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-net-1.0
+make depend
+make validate
+make reformat
+make couchbase-sdk-net.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-net-1.0/couchbase-sdk-net-ready.xml couchbase-sdk-net-1.0
+
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-net-1.1
+make depend
+make validate
+make reformat
+make couchbase-sdk-net.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-net-1.1/couchbase-sdk-net-ready.xml couchbase-sdk-net-1.1
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-net-1.2
+make depend
+make validate
+make reformat
+make couchbase-sdk-net.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-net-1.2/couchbase-sdk-net-ready.xml couchbase-sdk-net-1.2
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-net-1.2
+make depend
+make validate
+make reformat
+make couchbase-sdk-net.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-net-1.2/couchbase-sdk-net-ready.xml couchbase-sdk-net-1.2
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-php-1.0
+make depend
+make validate
+make reformat
+make couchbase-sdk-php.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-php-1.0/couchbase-sdk-php-ready.xml couchbase-sdk-php-1.0
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-php-1.1
+make depend
+make validate
+make reformat
+make couchbase-sdk-php.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-php-1.1/couchbase-sdk-php-ready.xml couchbase-sdk-php-1.1
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-ruby-1.0
+make depend
+make validate
+make reformat
+make couchbase-sdk-ruby.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-ruby-1.0/couchbase-sdk-ruby-ready.xml couchbase-sdk-ruby-1.0
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-ruby-1.1
+make depend
+make validate
+make reformat
+make couchbase-sdk-ruby.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-ruby-1.1/couchbase-sdk-ruby-ready.xml couchbase-sdk-ruby-1.1
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-ruby-1.2
+make depend
+make validate
+make reformat
+make couchbase-sdk-ruby.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-ruby-1.2/couchbase-sdk-ruby-ready.xml couchbase-sdk-ruby-1.2
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/hadoop-plugin
+make depend
+make validate
+make reformat
+make hadoop-plugin.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/hadoop-plugin/hadoop-plugin-ready.xml hadoop-plugin
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/hadoop-plugin-1.1
+make depend
+make validate
+make reformat
+make hadoop-plugin.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/hadoop-plugin-1.1/hadoop-plugin-ready.xml hadoop-plugin-1.1
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-python-1.0
+make depend
+make validate
+make reformat
+make couchbase-sdk-python.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-python-1.0/couchbase-sdk-python-ready.xml couchbase-sdk-python-1.0
+
+cd /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-essentials
+make depend
+make validate
+make reformat
+make couchbase-sdk-essentials.pdf
+cd ~/Desktop/docs-ng/
+java -jar ~/Desktop/throwdown.jar /Users/kzeller/Couchbase_docs/docs/Couchbase/products/couchbase/couchbase-sdk-essentials/couchbase-sdk-essentials-ready.xml couchbase-sdk-essentials
+
+
+
+cd ~/Desktop/docs-ng/
+
+nanoc
+
