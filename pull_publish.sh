b9a5c3a822e5707c0d534527d15214a68ba18995
Aaron Miller <apage43@ninjawhale.com>
2013-07-29 14:07:51 -0700

    only deploy master branch to s3

--- pull_publish.sh
+++ pull_publish.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
-
-cd /home/docs/docs-ng
+#!/bin/bash
+cd `dirname $0`
+BRANCH=`git symbolic-ref --short`
+echo "On branch ${BRANCH}, doing git pull"
 git pull
+echo "Building site..."
 bundle exec nanoc
-s3cmd sync -P output/ s3://docs-couchbase
+if [ "${BRANCH}" == "master" ]
+then
+    echo "On master branch, deploying to docs.couchbase.com"
+    s3cmd sync -P output/ s3://docs.couchbase.com/
+fi
