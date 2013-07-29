1fb25e45a07c8ca12eabf40c9f6d07f15beed713
Aaron Miller <apage43@ninjawhale.com>
2013-07-29 14:14:55 -0700

    more compatible branch name finding

--- pull_publish.sh
+++ pull_publish.sh
@@ -4,12 +4,12 @@ then
     source ~/cronenv
 fi
 cd `dirname $0`
-BRANCH=`git symbolic-ref --short`
-echo "On branch ${BRANCH}, doing git pull"
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+echo "On branch ${branch}, doing git pull"
 git pull
 echo "Building site..."
 bundle exec nanoc
-if [ "${BRANCH}" == "master" ]
+if [ "${branch}" == "master" ]
 then
     echo "On master branch, deploying to docs.couchbase.com"
     s3cmd sync -P output/ s3://docs.couchbase.com/
