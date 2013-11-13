e30e21c4d8fbb429dbe1f63210be8dec167ea2ae
Aaron Miller <apage43@ninjawhale.com>
2013-11-13 15:30:26 -0800

    three levels of left-nav

--- content/assets/js/inner_nav.js
+++ content/assets/js/inner_nav.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
   $("#sidenav li").on('activate', function() {
     $(this).addClass('active');
   });
-  $("#sidenav > li > a").click(function(){
+  $("#sidenav li > a").click(function(){
     $(this).parent().addClass("active").siblings().removeClass("active");
   });
   $("table").addClass('table');
