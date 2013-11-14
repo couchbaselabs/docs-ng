da89fb16041d0f5965a5c0500b6179c92d7c1170
Aaron Miller <apage43@ninjawhale.com>
2013-11-13 20:01:37 -0800

    fix scroll spy for navigation

--- content/assets/js/inner_nav.js
+++ content/assets/js/inner_nav.js
@@ -1,9 +1,3 @@
-var scrollup = _.throttle(function () {
-  $('[data-spy="scroll"]').each(function() {
-    $(this).scrollspy('refresh');
-  });
-}, 1000);
-
 $(document).ready(function () {
   $("#sidenav").affix({
     offset: { top: 20 }
@@ -11,8 +5,10 @@ $(document).ready(function () {
   $("#sidenav li").on('activate', function() {
     $(this).addClass('active');
   });
-  $("#sidenav li > a").click(function(){
-    $(this).parent().addClass("active").siblings().removeClass("active");
-  });
+//  $("#sidenav li > a").click(function(){
+//    $(this).parent().addClass("active").siblings().removeClass("active");
+//  });
   $("table").addClass('table');
+
+  $('body').scrollspy({target: '#outernav'});
 });
