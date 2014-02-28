3d5e6e514f21ac0c42ccab42c9466915272793f9
Sergey Avseyev <sergey.avseyev@gmail.com>
2014-02-28 21:24:17 +0300

    Use index YAML file

--- lib/extensions/item.rb
+++ lib/extensions/item.rb
@@ -10,4 +10,30 @@ class Nanoc::Item
   def toc
     @toc ||= Array.new
   end
+
+  def self.hierarchy
+    @hierarchy ||= begin
+                     filename = File.join(File.dirname(__FILE__), "..", "..", "hierarchy.yml")
+                     YAML.load(File.read(filename))
+                   end
+  end
+
+  def position
+    @position ||= begin
+                    position = []
+                    tree = self.class.hierarchy
+                    parts = raw_filename.split("/")
+                    parts.each do |p|
+                      node = tree.find do |t|
+                        (t.is_a?(Hash) && t.has_key?(p)) || t == p
+                      end
+                      if node.nil?
+                        raise "Cannot find #{raw_filename.inspect} in hierarchy"
+                      end
+                      position << tree.index(node)
+                      tree = node[p] if node.is_a?(Hash)
+                    end
+                    position
+                  end
+  end
 end
